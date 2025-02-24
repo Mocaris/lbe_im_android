9afd3d8b9cbcb1ffe9d5ef2a736ba3f2d369ac76
mz-main-12 <mz-main-12@gmail.com>
2025-02-24 14:58:39 +0800

    feat: i18n

--- LbeIMSdk/src/main/java/com/lbe/imsdk/ui/presentation/viewmodel/ChatScreenViewModel.kt
+++ LbeIMSdk/src/main/java/com/lbe/imsdk/ui/presentation/viewmodel/ChatScreenViewModel.kt
@@ -81,10 +81,8 @@ import kotlinx.coroutines.withContext
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.MultipartBody
 import okhttp3.OkHttpClient
-import okhttp3.RequestBody.Companion.asRequestBody
 import retrofit2.HttpException
 import java.io.ByteArrayOutputStream
-import java.io.File
 import java.io.IOException
 import java.security.MessageDigest
 import java.time.Duration

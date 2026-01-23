43de4f41ad02746b30be416021d9160354a1b1a4
Mocaris <imocaris@outlook.com>
2026-01-23 19:15:08 +0800

    fix: BI-719

--- LbeIMSdk/src/main/java/com/lbe/imsdk/ui/presentation/viewmodel/ChatScreenViewModel.kt
+++ LbeIMSdk/src/main/java/com/lbe/imsdk/ui/presentation/viewmodel/ChatScreenViewModel.kt
@@ -403,8 +403,8 @@ class ChatScreenViewModel(application: Application) : AndroidViewModel(applicati
     fun loadHistory() {
         viewModelScope.launch(Dispatchers.IO) {
             checkNeedSyncRemote()
-            syncPageInfo()
-            filterLocalMessages()
+//            syncPageInfo()
+//            filterLocalMessages()
         }
     }
 
@@ -461,7 +461,7 @@ class ChatScreenViewModel(application: Application) : AndroidViewModel(applicati
             REALM,
             "分页 ---->>> ,sessionId: $lbeSession, currentSessionTotalPages: $currentSessionTotalPages, currentPage: $currentPage, seq: $seq"
         )
-        if ((currentSessionTotalPages != 0 && currentPage >= currentSessionTotalPages) || currentPage < 1) return
+        if ((currentSessionTotalPages != 0 && currentPage > currentSessionTotalPages) || currentPage < 1) return
 
         val cacheMessages = IMLocalRepository.filterMessages(lbeSession)
         val subList = pagination(cacheMessages)
